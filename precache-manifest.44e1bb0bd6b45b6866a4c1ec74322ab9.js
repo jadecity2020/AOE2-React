c1d42762a748e644c3b67d9adc30a004f5664ea6
Jim Chen <jimchen@Jims-Air.fios-router.home>
2020-07-01 16:23:04 -0400

    Updates

---
+++ precache-manifest.44e1bb0bd6b45b6866a4c1ec74322ab9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60059156c05861b67464997e166eaa77",
+    "url": "/AOE2-React/index.html"
+  },
+  {
+    "revision": "f9f330bff6d7a90453e5",
+    "url": "/AOE2-React/static/css/main.920cd085.chunk.css"
+  },
+  {
+    "revision": "95ab74f54b59bec12921",
+    "url": "/AOE2-React/static/js/2.c0ecfc1d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/AOE2-React/static/js/2.c0ecfc1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9f330bff6d7a90453e5",
+    "url": "/AOE2-React/static/js/main.c1e63b63.chunk.js"
+  },
+  {
+    "revision": "ae89f35780a16e143900",
+    "url": "/AOE2-React/static/js/runtime-main.0c38797b.js"
+  }
+]);
